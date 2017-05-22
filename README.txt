f28390d87f53f61d9288614ce56b3cff314f23e0
DESKTOP-O42JDTJ\Bouke <bouke.stam@kubion.nl>
2017-05-22 15:43:02 +0200

    Changes

---
+++ README.txt
@@ -0,0 +1,3 @@
+Open index.html met een browser (chrome werkt in ieder geval).
+Verander de waardes en de grafiek update automatisch.
+Verander de scherm grootte om te zien dat het responsive is.
--- main.js
+++ main.js
@@ -12,15 +12,20 @@ function update(){
 	// Retrieve values
 	var timespan_visit_times = $("#timespan_visit_times option:selected").val();
 	var visit_times = $("#visit_times").val();
-	if(visit_times < 1){$("#visit_times").val(1);}
+	if(visit_times < 1){
+		visit_times = 1;
+		$("#visit_times").val(1);
+	}
 
 	var average_spending = $("#average_spending").val();
 	var valuta = $("#valuta option:selected").val();
-	if(average_spending < 0.01){$("#average_spending").val(0.01);}
+	if(average_spending < 0.01){
+		average_spending = 0.01;
+		$("#average_spending").val(0.01);
+	}
 	
 	var interest = $("#interest").val();
 	var timespan_interest = $("#timespan_interest option:selected").val();
-	if(interest < 0){$("#interest").val(0);}
 	
     // Calculate interest
 	var visitTimesEachYear = visit_times * timespan_visit_times;
